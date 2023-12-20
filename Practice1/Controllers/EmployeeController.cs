c204ab900bfba51046f09edcf8a6bd8a1d9a579e
CryAndDo <mrflesh554@gmail.com>
2023-12-20 14:20:11 +0200

    Добавьте файлы проекта.

---
+++ Practice1/Controllers/EmployeeController.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Mvc;
+using Practice1.Models;
+using System.Diagnostics;
+
+namespace Practice1.Controllers
+{
+    [Route("api/[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+        private DataContext db;
+        public EmployeeController(DataContext ctx)
+        {
+            db = ctx;
+        }
+        [HttpGet]
+        public IEnumerable<Employee> GetEmployee()
+        {
+            return db.Employees.ToList();
+        }
+        [HttpGet("{id}")]
+        public Employee GetEmployees(int id)
+        {
+            return db.Employees.Where(p => p.IdEmpleyee == id).FirstOrDefault()!;
+        }
+        [HttpPost]
+        public void SaveEmployees([FromBody] Employee employee)
+        {
+            db.Employees.Add(employee);
+            db.SaveChanges();
+        }
+        [HttpPut]
+        public void UpdateEmployee([FromBody] Employee employee)
+        {
+            db.Employees.Update(employee);
+            db.SaveChanges();
+        }
+        [HttpDelete("{id}")]
+        public void DeleteEmployee(long id)
+        {
+            db.Employees.Remove(db.Employees.Where(p => p.IdEmpleyee == id).FirstOrDefault()!);
+            db.SaveChanges();
+        }
+    }
+}
---
+++ Practice1/Controllers/GuestController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Practice1.Models;
+
+namespace Practice1.Controllers
+{
+    [Route("api/[controller]")]
+    public class GuestController : ControllerBase
+    {
+        private DataContext db;
+        public GuestController(DataContext ctx)
+        {
+            db = ctx;
+        }
+        [HttpGet]
+        public IEnumerable<Guest> GetGuest()
+        {
+            return db.Guests.ToList();
+        }
+        [HttpGet("{id}")]
+        public Guest GetGuest(int id)
+        {
+            return db.Guests.Where(p => p.IdGuest == id).FirstOrDefault()!;
+        }
+        [HttpPost]
+        public void SaveGuest([FromBody] Guest guest)
+        {
+            db.Guests.Add(guest);
+            db.SaveChanges();
+        }
+        [HttpPut]
+        public void UpdateGuest([FromBody] Guest guest)
+        {
+            db.Guests.Update(guest);
+            db.SaveChanges();
+        }
+        [HttpDelete("{id}")]
+        public void DeleteGuest(long id)
+        {
+            db.Guests.Remove(db.Guests.Where(p => p.IdGuest == id).FirstOrDefault()!);
+            db.SaveChanges();
+        }
+    }
+}
